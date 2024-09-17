00ba46aed9b601cdd2db54521bce349e7a862a1a
My Name <thesame@email.com>
2024-09-17 12:43:47 +0800

    starting point of coding: from navbar, sidebar, to the first fetching of popular movies from the API

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+
+import { useGetMoviesQuery } from "../../services/TMDB";
+import { MovieList } from "..";
 
 const Movies = () => {
-  console.log("Movies");
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  return <div>Movies</div>;
+  if (isFetching) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies match the name.
+          <br />
+          Search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return "an error has occured.";
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default Movies;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect } from "react";
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  ListItemIcon,
+  Box,
+  CircularProgress,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useTheme } from "@mui/styles";
+import useStyles from "./styles";
+
+const categories = [
+  { label: "Popular", value: "popular" },
+  { label: "Top Rated", value: "top_rated" },
+  { label: "Upcoming", value: "upcoming" },
+];
+
+const demoCategories = [
+  { label: "Comedy", value: "comedy" },
+  { label: "Action", value: "action" },
+  { label: "Horror", value: "horror" },
+  { label: "Animation", value: "animation" },
+];
+
+const blueLogo =
+  "https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png";
+const redLogo =
+  "https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png";
+
+const Sidebar = ({ setMobileOpen }) => {
+  const theme = useTheme();
+  const classes = useStyles();
+
+  return (
+    <>
+      <Link to="/" className={classes.imageLink}>
+        <img
+          className={classes.image}
+          src={theme.palette.mode === "light" ? redLogo : blueLogo}
+          alt="Artie Logo"
+        />
+      </Link>
+      <Divider />
+      <List>
+        {/* Destructure values in categories */}
+        <ListSubheader>Categories</ListSubheader>
+        {categories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItem onClick={() => {}} button>
+              <ListItemIcon>
+                <img
+                  src={redLogo}
+                  className={classes.genreImages}
+                  height={30}
+                  alt="Category Logo"
+                />
+              </ListItemIcon>
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        <ListSubheader>Genres</ListSubheader>
+        {demoCategories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItem onClick={() => {}} button>
+              <ListItemIcon>
+                <img
+                  src={redLogo}
+                  className={classes.genreImages}
+                  height={30}
+                  alt="Category Logo"
+                />
+              </ListItemIcon>
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </>
+  );
+};
+
+export default Sidebar;
